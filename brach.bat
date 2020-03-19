14874c092904a249a7008bcb31597b7d951e4d37
julian john <mail2julian1996@gmail.com>
2020-03-19 16:55:26 +0530

    'add'

--- brach.bat
+++ brach.bat
@@ -3,5 +3,5 @@ git init
 git checkout -b latest
 git add .
 git commit -m 'add'
-git remote add late https://github.com/mail2julian/butt.git
-git push -u late master
+git remote add origin https://github.com/mail2julian/butt.git
+git push -u origin master
