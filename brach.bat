7b5daadf978b59c0f70f16661ccf038df7a687f5
julian john <mail2julian1996@gmail.com>
2020-03-19 22:40:32 +0530

    'add'

--- brach.bat
+++ brach.bat
@@ -1,4 +1,12 @@
 git init
+git add -A
+git config --global user.email"mail2julian1996@gmail.com"
+gti config --global user.name"mail2julian"
+git commit -m "asdas"
+git remote add origin link
+git branch branchname
+git checkout branchname
+ 
 git checkout -b lat 
 git add .
 git commit -m "latest"
