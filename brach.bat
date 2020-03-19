ddd8f64bacbe5ef4a6f72821c2f188b7ce9e078f
julian john <mail2julian1996@gmail.com>
2020-03-19 22:06:46 +0530

    latest

--- brach.bat
+++ brach.bat
@@ -2,5 +2,5 @@ git init
 gti checkout -b lat 
 git add .
 git commit -m "latest"
-git remote add orig https://github.com/mail2julian/butt.git
+git remote add ori https://github.com/mail2julian/butt.git
 git push -u origin lat
