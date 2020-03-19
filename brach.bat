45e538e71509c8257d81e913d4d9a9283476de75
julian john <mail2julian1996@gmail.com>
2020-03-19 22:41:56 +0530

    asdas

--- brach.bat
+++ brach.bat
@@ -3,12 +3,7 @@ git add -A
 git config --global user.email"mail2julian1996@gmail.com"
 gti config --global user.name"mail2julian"
 git commit -m "asdas"
-git remote add origin link
+git remote add origin https://github.com/mail2julian/butt.git
 git branch branchname
 git checkout branchname
- 
-git checkout -b lat 
-git add .
-git commit -m "latest"
-git remote add ori https://github.com/mail2julian/butt.git
-git push -u ori lat
+git push -u origin branchname
